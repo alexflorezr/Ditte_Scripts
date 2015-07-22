ab67cb59321eaab80f4a50899d8f082ece621bfd
dmtruelsen <dmtruelsen@snm.ku.dk>
2015-07-22 22:34:36 +0200

    To Do climate change velocity

---
+++ ToDo_Velocity.R
@@ -0,0 +1,6 @@
+1) All the coordinates have to be correct 
+  a. Check coordinates for each species from the maps
+    a1. Which ones are ready and which are not - Is it easy to fix or do not work witht that specific species
+
+2) Modify the velocity script in order to work with the database for both DNA and fossil
+  a. Add ranking to the script
